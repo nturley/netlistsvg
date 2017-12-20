77b26d4d94000a35b9674f2b4fc06512f145dfcf
Tim 'mithro' Ansell <mithro@mithis.com>
2017-12-20 12:06:44 +0100

    Make the TOP in examples match file name.
    (Makes the Makefile easier.)

--- examples/carry4whole.v
+++ examples/carry4whole.v
@@ -1,4 +1,4 @@
-module CARRY4(output [3:0] CO, O, input CI, CYINIT, input [3:0] DI, S);
+module CARRY4WHOLE(output [3:0] CO, O, input CI, CYINIT, input [3:0] DI, S);
   assign O = S ^ {CO[2:0], CI | CYINIT};
   assign CO[0] = S[0] ? CI | CYINIT : DI[0];
   assign CO[1] = S[1] ? CO[0] : DI[1];
