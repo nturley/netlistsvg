16cbe6f60cc429047fc12c44bd6bc6e28bae7b8b
Neil Turley <neilpturley@gmail.com>
2017-12-25 09:41:05 -0600

    push file system operations from lib to bin #10

--- bin/netlistsvg.js
+++ bin/netlistsvg.js
@@ -2,10 +2,21 @@
 'use strict';
 
 var lib = require('../lib'),
-    yargs = require('yargs');
+    yargs = require('yargs'),
+    fs = require('fs-extra');
 
 var argv = yargs
             .demand(1)
             .usage('usage: $0 input_json_file [-o output_svg_file] [--skin skin_file]')
             .argv;
-lib.render(argv._[0], argv.o, argv.skin);
+
+// set default values for optional args
+var skinPath = argv.skin || __dirname + '/' + 'default.svg';
+var outputPath = argv.o || 'out.svg';
+
+var skin_data = fs.readFileSync(skinPath, 'utf-8');
+var yosys_netlist = fs.readJsonSync(argv._[0]);
+lib.netlistSvg(yosys_netlist, skin_data, function(error, result) {
+    if (error) throw error;
+    fs.writeFileSync(outputPath, result, 'utf-8');
+});
--- test/test-all.js
+++ test/test-all.js
@@ -1,6 +1,17 @@
-var lib = require('../lib');
+var lib = require('../lib'),
+    fs = require('fs-extra');
+
+var skin_data = fs.readFileSync('lib/default.svg', 'utf-8');
+
+function drawTest(testName) {
+    var yosys_netlist = fs.readJsonSync('test/' + tests[i] + '.json');
+    lib.netlistSvg(yosys_netlist, skin_data, function(error, result) {
+        if (error) throw error;
+        fs.writeFileSync('test/' + testName + '.svg', result, 'utf-8');
+    }); 
+}
 
 var tests = ['generics', 'ports_splitjoin', 'up3down5', 'mux4'];
 for (var i in tests) {
-    lib.render('test/' + tests[i] + '.json', 'test/' + tests[i] + '.svg');
+    drawTest(tests[i]);
 }
