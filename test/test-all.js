19664793a985365a26503c6458089455df63cd6a
Neil Turley <neilpturley@gmail.com>
2017-12-25 17:45:29 -0600

    Revert "push file system operations from lib to bin #10"
    This reverts commit 16cbe6f60cc429047fc12c44bd6bc6e28bae7b8b.

--- test/test-all.js
+++ test/test-all.js
@@ -1,17 +1,6 @@
-var lib = require('../lib'),
-    fs = require('fs-extra');
-
-var skin_data = fs.readFileSync('lib/default.svg', 'utf-8');
-
-function drawTest(testName) {
-    var yosys_netlist = fs.readJsonSync('test/' + tests[i] + '.json');
-    lib.netlistSvg(yosys_netlist, skin_data, function(error, result) {
-        if (error) throw error;
-        fs.writeFileSync('test/' + testName + '.svg', result, 'utf-8');
-    }); 
-}
+var lib = require('../lib');
 
 var tests = ['generics', 'ports_splitjoin', 'up3down5', 'mux4'];
 for (var i in tests) {
-    drawTest(tests[i]);
+    lib.render('test/' + tests[i] + '.json', 'test/' + tests[i] + '.svg');
 }
