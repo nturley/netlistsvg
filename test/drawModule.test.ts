d08fc54a4c48237a279672eb3f67dd95601bede4
Neil Turley <neilpturley@gmail.com>
2019-04-09 20:03:56 -0500

    add another remove dummy edge (should fail)

--- test/drawModule.test.ts
+++ test/drawModule.test.ts
@@ -2,12 +2,95 @@ import { ElkModel } from '../lib/elkGraph';
 import { removeDummyEdges } from '../lib/drawModule';
 import _ = require('lodash');
 
-test('remove dummy edges', () => {
+test('remove dummy edges outputs', () => {
+    const e2end = {x: 249, y: 162};
+    const e3end = {x: 249, y: 97};
+    const e4end = {x: 249, y: 32};
+    const dummyPos = {x: 214, y: 32};
+    const highJunct = {x: 224, y: 32};
+    const lowJunct = {x: 224, y: 97};
+
+    const testGraph: ElkModel.Graph = {
+        id: 'fake id',
+        children: [],
+        edges:
+            [{
+                id: 'e2',
+                source: '$d_0',
+                sourcePort: '$d_0.p',
+                target: 'o1',
+                targetPort: 'o1.A',
+                sections: [
+                    {
+                        id: 'e2_s0',
+                        startPoint: dummyPos,
+                        endPoint: e2end,
+                        bendPoints: [
+                            {x: 224, y: 32},
+                            {x: 224, y: 162},
+                        ],
+                    },
+                ],
+                junctionPoints: [highJunct],
+            },
+            {
+                id: 'e3',
+                source: '$d_0',
+                sourcePort: '$d_0.p',
+                target: 'o2',
+                targetPort: 'o2.A',
+                sections: [
+                    {
+                        id: 'e3_s0',
+                        startPoint: dummyPos,
+                        endPoint: e3end,
+                        bendPoints: [
+                            {x: 224, y: 32},
+                            {x: 224, y: 97},
+                        ],
+                    },
+                ],
+                junctionPoints: [lowJunct],
+            },
+            {
+                id: 'e4',
+                source: '$d_0',
+                sourcePort: '$d_0.p',
+                target: 'o3',
+                targetPort: 'o3.A',
+                sections: [
+                    {
+                        id: 'e4_s0',
+                        startPoint: dummyPos,
+                        endPoint: e4end,
+                    },
+                ],
+            },
+        ],
+    };
+    removeDummyEdges(testGraph);
+    const e2 = testGraph.edges.find((edge) => edge.id === 'e2');
+    const e3 = testGraph.edges.find((edge) => edge.id === 'e3');
+    const e4 = testGraph.edges.find((edge) => edge.id === 'e4');
+    // edge end points should stay the same
+    expect(e2.sections[0].endPoint).toEqual(e2end);
+    expect(e3.sections[0].endPoint).toEqual(e3end);
+    expect(e4.sections[0].endPoint).toEqual(e4end);
+    // edge start points should be lowjunct
+    expect(e2.sections[0].startPoint).toEqual(highJunct);
+    expect(e3.sections[0].startPoint).toEqual(highJunct);
+    expect(e4.sections[0].startPoint).toEqual(highJunct);
+    const junctionPoints = _.flatMap(testGraph.edges, (edge) => edge.junctionPoints || []);
+    expect(junctionPoints.length).toEqual(1);
+});
+
+test('remove dummy edges inputs', () => {
     // this test case came from hyperedges.json
     const e5Start = {x: 159, y: 162};
     const e6Start = {x: 159, y: 97};
     const e7Start = {x: 159, y: 32};
     const initEnd = {x: 202, y: 97};
+    const junctPoint = {x: 177, y: 97};
     const testGraph: ElkModel.Graph = {
         id: 'fake id',
         children: [],
@@ -66,10 +149,7 @@ test('remove dummy edges', () => {
                         ],
                     },
                 ],
-                junctionPoints:
-                [
-                    {x: 177, y: 97},
-                ],
+                junctionPoints: [junctPoint],
             },
         ],
     };
@@ -77,16 +157,15 @@ test('remove dummy edges', () => {
     const e5 = testGraph.edges.find((edge) => edge.id === 'e5');
     const e6 = testGraph.edges.find((edge) => edge.id === 'e6');
     const e7 = testGraph.edges.find((edge) => edge.id === 'e7');
+    // edge start points should stay the same
     expect(e5.sections[0].startPoint).toEqual(e5Start);
     expect(e6.sections[0].startPoint).toEqual(e6Start);
     expect(e7.sections[0].startPoint).toEqual(e7Start);
-    expect(e5.sections[0].endPoint.x).not.toEqual(initEnd);
-    expect(e6.sections[0].endPoint.x).not.toEqual(initEnd);
-    expect(e7.sections[0].endPoint.x).not.toEqual(initEnd);
+    // edge end points should now be the junction
+    expect(e5.sections[0].endPoint).toEqual(junctPoint);
+    expect(e6.sections[0].endPoint).toEqual(junctPoint);
+    expect(e7.sections[0].endPoint).toEqual(junctPoint);
+    // there should still be one junction
     const junctionPoints = _.flatMap(testGraph.edges, (edge) => edge.junctionPoints || []);
     expect(junctionPoints.length).toEqual(1);
 });
-
-function findEdge(graph: ElkModel.Graph, id: string): ElkModel.Edge {
-    return graph.edges.find((edge) => edge.id === id);
-}
