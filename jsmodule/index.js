7c98988c40a67a84d14a7bb916dbcb467abae04d
Neil Turley <neilpturley@gmail.com>
2019-04-24 00:56:59 -0500

    create browser module

---
+++ jsmodule/index.js
@@ -0,0 +1,30 @@
+const lib = require('../built');
+const fs = require('fs');
+const json5 = require('json5');
+const Ajv = require('ajv');
+var ajv = new Ajv({allErrors: true, jsonPointers: true});
+require('ajv-errors')(ajv);
+
+const digital = fs.readFileSync(__dirname + '/../lib/default.svg', 'utf8');
+const analog = fs.readFileSync(__dirname + '/../lib/analog.svg', 'utf8');
+const exampleDigital = fs.readFileSync(__dirname + '/../test/digital/up3down5.json');
+const exampleAnalog = fs.readFileSync(__dirname + '/../test/analog/and.json');
+const schema = fs.readFileSync(__dirname + '/../lib/yosys.schema.json5');
+const exampleDigitalJson = json5.parse(exampleDigital);
+const exampleAnalogJson = json5.parse(exampleAnalog);
+
+function render(skinData, netlistData, cb) {
+    var valid = ajv.validate(json5.parse(schema), netlistData);
+    if (!valid) {
+        throw Error(JSON.stringify(ajv.errors, null, 2));
+    }
+    return lib.render(skinData, netlistData, cb);
+}
+
+module.exports = {
+    render: render,
+    digitalSkin: digital,
+    analogSkin: analog,
+    exampleDigital: exampleDigitalJson,
+    exampleAnalog: exampleAnalogJson
+};
